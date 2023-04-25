28e84973d980929fb8d45fff3f2c0bc5e55964cc
SOFIAMEDINAA <128167706+SOFIAMEDINAA@users.noreply.github.com>
2023-04-25 12:27:01 -0300

    Add files via upload

---
+++ ejercicio 2/ejercicio-2-login/algoritmo.js
@@ -0,0 +1,29 @@
+const BITCOIN_A_PRECIO_ETHERUM = 13.54;
+const BITCOIN_A_PRECIO_DOLAR = 39685.40;
+
+/* let bitcoinIngresado = prompt ("Dame una cantidad de Bitcoin");
+let result =window.confirm("si aceptas das a dolar si negas das a ETH") */
+ 
+const ingreseCantBitcoin = document.querySelector("#ingrese-cant-bitcoin");
+const ingreseCantDolares = document.querySelector("#ingrese-cant-dolares");
+
+let  ingresecantbitcoin = "def bitcoin" ;
+ let ingresecantdolares = "def dolares";
+ 
+function result() {
+    ingresecantbitcoin = inputBitcoin.value;
+    ingresecantdolares= inputDolares.value;
+
+    if ( (ingreseCantDolares == InputDolares) ) {
+        
+    } 
+    
+}
+/* if (result == true) {
+    alert (("tu cantidad de dolares es")+(bitcoinIngresado*BITCOIN_A_PRECIO_DOLAR)
+    )
+} else {
+    alert ( ("Tu cantidad de ETH es") + (bitcoinIngresado * BITCOIN_A_PRECIO_ETHERUM))
+}
+ */
+/* antu y sofia */
